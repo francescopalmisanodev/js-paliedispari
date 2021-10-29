0d584be5a46109e52af226e6c5726df3769846bf
francescopalmisanodev <francescopalmisano.dev@gmail.com>
2021-10-29 17:13:39 +0200

    odd or even (to fix)

---
+++ main.js
@@ -0,0 +1,24 @@
+let scelta,num,nrand;
+
+do{   
+    scelta=prompt("scegli pari o dispari:");
+    scelta=scelta.toLowerCase();
+}while(!(scelta==="pari"||scelta==="dispari"))
+do{   
+    num=parseInt(prompt("inserisci un numero da 1 a 5:"));
+}while(isNaN(num) || !(num>=0 && num<=5))
+
+nrand=rnd(5);
+nrand+=num;
+check(nrand);
+
+function rnd(n){
+    return(math.floor(Math.random()*n));
+}
+function check(n){
+    if(n%2===0 && scelta==="pari" || !(n%2===0) && scelta==="dispari"){
+        console.log("hai vinto");
+    }else {
+        console.log("hai perso");
+    }
+}
